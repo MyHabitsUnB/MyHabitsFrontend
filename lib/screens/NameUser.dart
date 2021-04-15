baa30ed97a41d4a8149890c2a0e0d7b7e7fa1c38
Filipe Dias <lipao.dias@hotmail.com>
2021-04-15 20:43:16 -0300

    :art: Improving app design

--- lib/screens/NameUser.dart
+++ lib/screens/NameUser.dart
@@ -36,66 +36,83 @@ class _NameUserState extends State<NameUser> {
             child: SafeArea(
                 child: SingleChildScrollView(
                     child: Column(
-              children: [
-                SizedBox(height: _height * 0.02),
-                // SizedBox(
-                //   height: 3,
-                // ),
-                // Expanded(
-                //   child: Image.asset(
-                //     "images/SmartMeat.png",
-                //     fit: BoxFit.fitHeight,
-                //     height: 90.0,
-                //     alignment: Alignment.topCenter,
-                //   ),
-                // ),
-                ImageBanner("images/...png"),
-                TextSection("\nDigite seu nome\n no campo abaixo!"),
-                SizedBox(height: _height * 0.04),
-                Container(
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 15.0, vertical: 15),
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          TextField(
+                      children: [
+                        SizedBox(height: _height * 0.02),
+//                ImageBanner(""),
+                        Image.asset(
+                          "assets/images/undraw_font.png",
+                          fit: BoxFit.cover,
+                          height: 210.0,
+                        ),
+                        Column(
+                          children: <Widget>[
+                            Text(
+                              "Comece agora mesmo a\n utilizar o MyHabits",
+                              style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 25.0,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                            SizedBox(
+                              height: 1,
+                            ),
+                            Text(
+                              "\nInsira seu nome\n no campo abaixo e comece\n a usar o MyHabits!",
+                              style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 15.0,
+                                //fontWeight: FontWeight.bold,
+                              ),
+                              maxLines: 4,
+                              overflow: TextOverflow.ellipsis,
+                              textAlign: TextAlign.center,
+                            ),
+                          ]
+                        ),
+                        SizedBox(height: _height * 0.04),
+                        Container(
+                          padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 15),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            TextField(
                             onChanged: (text) {
                               setName(text);
                             },
                             decoration: InputDecoration(
                               // border: InputBorder.none,
-                              hintText: 'Insira seu nome',
+                              hintText: 'Insira seu nome aqui :)',
                               labelText: "Nome",
                               labelStyle: TextStyle(
                                 height: 0,
-                                fontSize: 30,
+                                fontSize: 25,
                                 color: Colors.black,
                               ),
                             ),
                           )
                         ])),
-                SizedBox(height: _height * 0.05),
-                Container(
-                  alignment: Alignment(0.0, 0.9),
-                  padding: EdgeInsets.all(20.0),
-                  child: ButtonTheme(
-                      minWidth: 100.0,
-                      height: 60.0,
-                      child: FloatingActionButton(
-                        child: Text(
-                          '✔',
-                          style: TextStyle(
-                            fontSize: 30.0,
-                            color: Colors.white,
-                          ),
-                        ),
-                        backgroundColor: Colors.orange,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(50.0)),
-                        onPressed: () {
-                          Navigator.of(context).pushReplacement(
-                              MaterialPageRoute(builder: (context) => BottomNavScreen()));
+                //SizedBox(height: _height * 0.05),
+                        Container(
+                          alignment: Alignment(0.0, 0.9),
+                          padding: EdgeInsets.all(20.0),
+                          child: ButtonTheme(
+                            minWidth: 100.0,
+                            height: 60.0,
+                            child: FloatingActionButton(
+                              child: Text('✔',
+                              style: TextStyle(
+                                fontSize: 30.0,
+                                color: Colors.white,
+                              )),
+                              backgroundColor: Colors.orange,
+                              shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50.0)),
+                              onPressed: () {
+                                Navigator.of(context).pushReplacement(
+                                MaterialPageRoute(
+                                  builder: (context) => BottomNavScreen()));
                         },
                       )),
                 ),
--- lib/screens/welcome_page.dart
+++ lib/screens/welcome_page.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
-
 import 'NameUser.dart';
 
 class WelcomePage extends StatelessWidget {
+
+  int index = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //backgroundColor: Color(0xfff5faf6),
       body: DefaultTabController(
         length: choices.length,
+        initialIndex: 0,
         child: Builder(
           builder: (BuildContext context) => Padding(
             padding: const EdgeInsets.all(8.0),
@@ -29,38 +33,55 @@ class WelcomePage extends StatelessWidget {
                     ),
                   ),
                 ),
-                Container(
-                  alignment: Alignment(0.0, 0.9),
-                  padding: EdgeInsets.all(20.0),
-                  child: ButtonTheme(
-                      minWidth: 100.0,
-                      height: 60.0,
-                      child: RaisedButton(
-                        child: Text(
-                          'INICIAR',
-                          style: TextStyle(
-                            fontSize: 40.0,
-                            color: Colors.white,
-                          ),
-                        ),
-                        color: Colors.orange,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30.0)),
-                        onPressed: () {
-                          final TabController controller =
-                          DefaultTabController.of(context);
-                          if (!controller.indexIsChanging) {
-                            controller.animateTo(choices.length - 1);
-                          }
-                          Navigator.of(context).pushReplacement(
-                              MaterialPageRoute(
-                                  builder: (context) => NameUser()));
-                        },
-                      )),
-                ),
-                TabPageSelector(
-                  color: Colors.white,
-                  selectedColor: Colors.orange,
+                Row(
+                  children: <Widget>[
+                    SizedBox(
+                      width: 20,
+                    ),
+                    TabPageSelector(
+                      color: Colors.white,
+                      selectedColor: Colors.orange,
+                    ),
+                    SizedBox(
+                      width: 90,
+                    ),
+                    Container(
+                      alignment: Alignment(0.0, 0.9),
+                      padding: EdgeInsets.all(20.0),
+                      child: ButtonTheme(
+                          minWidth: 100.0,
+                          height: 60.0,
+                          child: RaisedButton(
+                            child: Text(
+                              'Próximo',
+                              style: TextStyle(
+                                fontSize: 40.0,
+                                color: Colors.white,
+                              ),
+                            ),
+                            color: Colors.orange,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0)),
+                            onPressed: () {
+                              final TabController controller =
+                              DefaultTabController.of(context);
+
+                              if(this.index == 2) {
+                                /*if (!controller.indexIsChanging) {
+                                  controller.animateTo(choices.length - 1);
+                                }*/
+                                Navigator.of(context).pushReplacement(
+                                    MaterialPageRoute(
+                                        builder: (context) => NameUser()));
+                              } else {
+                                DefaultTabController.of(context).animateTo(this.index + 1);
+                                this.index = this.index + 1;
+                              }
+
+                            },
+                          )),
+                    )
+                  ],
                 )
               ],
             ),
@@ -72,20 +93,33 @@ class WelcomePage extends StatelessWidget {
 }
 
 class Choice {
-  const Choice({this.text, this.imageFile});
+  const Choice({this.text, this.imageFile, this.description});
 
   final String text;
+  final String description;
   final imageFile;
 }
 
-const String text1 = "Pensando em adotar um estilo de vida saudável?";
-const String text2 = "Que tal estudar mais?";
-const String text3 = "Vamos Meditar?";
+const String text1 = "Adote um estilo\n de vida saudável";
+const String text2 = "Aperfeiçoe sua\n rotina de estudos";
+const String text3 = "Medite e descanse com MyHabits";
 
 const List<Choice> choices = const <Choice>[
-  const Choice(text: text1, imageFile: "images/intro1.png"),
-  const Choice(text: text2, imageFile: "images/intro2.png"),
-  const Choice(text: text3, imageFile: "images/intro3.jpg"),
+  const Choice(
+      text: text1,
+      imageFile: "assets/images/undraw_Dog_walking.png",
+      description:
+          'Utilize MyHabits e acompanhe o\n andamento de suas atividades de onde estiver.'),
+  const Choice(
+      text: text2,
+      imageFile: "assets/images/undraw_studying.png",
+      description:
+          'Estamos aprimorando o app, em breve você\n vai poder fazer tudo por aqui. Aguarde!'),
+  const Choice(
+      text: text3,
+      imageFile: "assets/images/undraw_mindfulness.png",
+      description:
+          'Programe seu calendário aqui no app para sempre separar um tempo do seu dia para lembrar '),
 ];
 
 class ChoiceCard extends StatelessWidget {
@@ -106,18 +140,37 @@ class ChoiceCard extends StatelessWidget {
           Image.asset(
             choice.imageFile,
             fit: BoxFit.cover,
-            height: 190.0,
+            height: 300.0,
           ),
           SizedBox(
-            height: 50,
+            height: 20,
           ),
           Expanded(
               child: Text(
-                choice.text,
-                maxLines: 4,
-                overflow: TextOverflow.ellipsis,
-                textAlign: TextAlign.center,
-              )),
+              choice.text,
+              style: const TextStyle(
+              color: Colors.black,
+              fontSize: 25.0,
+            ),
+            maxLines: 4,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+          )),
+          SizedBox(
+            height: 1,
+          ),
+          Expanded(
+              child: Text(
+              choice.description,
+              style: const TextStyle(
+              color: Colors.black,
+              fontSize: 15.0,
+              //fontWeight: FontWeight.bold,
+            ),
+            maxLines: 4,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+          )),
         ],
       ),
     );
